127a6d50fd0510884ca090af57467abbbfaab2be
j. jillian <judith.mediosdigitales@gmail.com>
2022-10-02 18:39:43 +0200

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { FaBeer, FaTimes, FaBars } from "react-icons/fa";
 import Logo from "../assets/logo.png";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+import "../style/navbar.css";
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
   return (
     <header>
       <nav className="navbar">
         <div className="logo">
           <Link to="/">
-            <img src={Logo} alt="website-logo" />
+            <img src={Logo} alt="" />
           </Link>
         </div>
-        <ul className="nav-menu">
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
           <li className="nav-item">
             <Link to="/" className="nav-link">
               Home
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/" className="nav-link">
-              About
+            <Link to="/pricing" className="nav-link">
+              Pricing
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/" className="nav-link">
-              Projects
+            <Link to="/faq" className="nav-link">
+              FAQ
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/" className="nav-link">
+            <Link to="/contact" className="nav-link">
               Contact
             </Link>
           </li>
         </ul>
-
-        <div className="hamburger">
-          <FaBars />
+        <div className="hamburger" onClick={handleClick}>
+          {click ? <FaTimes size={20} /> : <FaBars size={20} />}
         </div>
       </nav>
     </header>
