3e9b39f391615990741f665f3fb246b2d149375a
j. jillian <judith.mediosdigitales@gmail.com>
2022-10-06 20:32:37 +0200

    hero background

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,40 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 import Logo from "../assets/logo.png";
 import { FaBars, FaTimes } from "react-icons/fa";
-
+import { Link } from "react-router-dom";
 import "../style/navbar.css";
+import { useState } from "react";
 
 const Navbar = () => {
   const [click, setClick] = useState(false);
-  const handleClick = () => setClick(!click);
+  const handleOnclick = () => setClick(!click);
 
   return (
     <header>
       <nav className="navbar">
         <div className="logo">
-          <Link to="/">
-            <img src={Logo} alt="" />
-          </Link>
+          <img src={Logo} width={120} alt="website-logo" />
         </div>
+
         <ul className={click ? "nav-menu active" : "nav-menu"}>
           <li className="nav-item">
-            <Link to="/" className="nav-link">
-              Home
-            </Link>
+            <Link className="nav-link">About</Link>
           </li>
           <li className="nav-item">
-            <Link to="/pricing" className="nav-link">
-              Pricing
-            </Link>
+            <Link className="nav-link">Packages</Link>
           </li>
           <li className="nav-item">
-            <Link to="/faq" className="nav-link">
-              FAQ
-            </Link>
+            <Link className="nav-link">F&Q</Link>
           </li>
           <li className="nav-item">
-            <Link to="/contact" className="nav-link">
-              Contact
-            </Link>
+            <Link className="nav-link">Contact</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link">Sing In</Link>
           </li>
         </ul>
-        <div className="hamburger" onClick={handleClick}>
+
+        <div className="hamburger" onClick={handleOnclick}>
           {click ? <FaTimes size={20} /> : <FaBars size={20} />}
         </div>
       </nav>
